89cfdc3062e4aea60ae925e1c0ff315e1d7bc433
RicKPB <papeschihenrique0@gmail.com>
2024-04-24 01:49:58 -0300

    Atualização do codigos .php

--- IstDepartamento.php
+++ IstDepartamento.php
@@ -1,8 +1,8 @@
 <?php
     include 'conexao.php';
     $sql = "select * from departamento;";
-    $con = Conexao::conectar();
-    $lstDpto = $con->query($sql)
+    $con = Conexao::conectar(); 
+    $lstDpto = $con->query($sql); 
 ?>
 
 <!DOCTYPE html>
@@ -10,22 +10,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listar Departamentos<title>
+    <title>Listar Departamentos</title>
 </head>
 <body>
-    <h1>Listar Departamentos</h1>
+    <h1>Listar Departamentos</h1> 
     <table>
         <tr>
             <th>ID</th>
             <th>Descrição</th>
         </tr>
-
+        
         <?php foreach($lstDpto as $dpto) {?>
-            <tr>
-                <td> <?php echo $dpto['id']; ?> </td>
-                <td> <?php echo $dpto['descriacao']?> </td>
-            </tr> 
+           <tr>
+              <td> <?php echo $dpto['id']; ?> </td>
+              <td> <?php echo $dpto['descricao']?> </td>
+           </tr>
         <?php } ?>
     </table>
+    
 </body>
 </html>
